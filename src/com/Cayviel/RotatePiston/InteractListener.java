bf83c7146f7827af4dfafe05caf4c66757438149
Caleb Devine <Cayviel@gmail.com>
2011-08-15 01:18:58 -0400

    Fully fixed extended piston rotations
    Signed-off-by: Caleb Devine <Cayviel@gmail.com>

--- src/com/Cayviel/RotatePiston/InteractListener.java
+++ src/com/Cayviel/RotatePiston/InteractListener.java
@@ -8,7 +8,7 @@ import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.event.block.Action;
 
 public class InteractListener extends PlayerListener {
-
+	
 	byte clickeddirc(byte clickeddir){
 	switch (clickeddir){
 	case 0: return 1; 
@@ -30,15 +30,18 @@ public class InteractListener extends PlayerListener {
 			}
 		
 		if (PlayerWithPiston.getAction().equals(Action.RIGHT_CLICK_BLOCK)){
-			
-		Material Blocktype = PlayerWithPiston.getClickedBlock().getType();
 		
+		Material Blocktype = PlayerWithPiston.getClickedBlock().getType();
 		if ((Blocktype == Material.PISTON_BASE)||(Blocktype == Material.PISTON_STICKY_BASE)){
+			boolean sticky = false;
+			if (Blocktype == Material.PISTON_STICKY_BASE){sticky = true;}
 			BlockFace clickedface = PlayerWithPiston.getBlockFace();
-			Block piston = PlayerWithPiston.getClickedBlock();			
-			byte dir = piston.getData();
+			Block piston = PlayerWithPiston.getClickedBlock();
+			
+			byte dir = (byte) (piston.getData() % 8);
 			byte clickeddir;
-
+			byte dirsetto;
+			
 			if (clickedface == BlockFace.valueOf("NORTH")){clickeddir=4;}else{
 				if (clickedface == BlockFace.valueOf("EAST")){clickeddir=2;}else{
 					if (clickedface == BlockFace.valueOf("SOUTH")){clickeddir=5;}else{
@@ -52,19 +55,52 @@ public class InteractListener extends PlayerListener {
 					}
 				}
 			}
+			piston.setTypeId(0);
+			if (sticky==false){
+				piston.setType(Material.PISTON_BASE);
+			}else{
+				piston.setType(Material.PISTON_STICKY_BASE);
+			}
 			
 		if (!PlayerWithPiston.getPlayer().isSneaking()){
-			if (clickeddir == dir){
-				piston.setData(clickeddirc(clickeddir));	
-			}else{
-				piston.setData(clickeddir);
-			}
+				if (clickeddir == dir){
+					dirsetto=clickeddirc(clickeddir);
+				}else{
+					dirsetto=clickeddir;
+				}
 		}else{
 			while(((byte)((dir+1) % 6)==clickeddir)||((byte)((dir+1) % 6)==clickeddirc(clickeddir))){dir = (byte)((dir+1) % 6);} //don't try sides already accessible
-			piston.setData((byte)((dir+1) % 6));
+			dirsetto=(byte)((dir+1) % 6);
 			}
+		piston.setData(dirsetto);
+		
+		if (piston.isBlockPowered()){
+			if (piston.getRelative(convertdirtoface(dirsetto)).getType()==Material.AIR){
+			piston.setData((byte) (piston.getData()+8));
+			piston.getRelative(convertdirtoface(dirsetto)).setType(Material.PISTON_EXTENSION);
+			if (sticky == false){
+				piston.getRelative(convertdirtoface(dirsetto)).setData(dirsetto);
+			}else{
+				piston.getRelative(convertdirtoface(dirsetto)).setData((byte) (dirsetto+8));
+			}
+			
+			}
+		}
 		}
 	}
 		
 	}
+	
+	private BlockFace convertdirtoface (byte dir){
+		switch (dir){
+		case 0: return BlockFace.DOWN;
+		case 1: return BlockFace.UP;
+		case 2: return BlockFace.EAST;
+		case 3: return BlockFace.WEST;
+		case 4: return BlockFace.NORTH;
+		case 5: return BlockFace.SOUTH;
+		default: return BlockFace.DOWN;
+		}
+	}
 }
+	
