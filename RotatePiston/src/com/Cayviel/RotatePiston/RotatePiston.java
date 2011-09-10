7441423e1159dca293fa7be9232f1021216151fe
Caleb Devine <Cayviel@gmail.com>
2011-09-10 13:52:55 -0400

    Added configurable wand.  Fixed null pointer call.

--- RotatePiston/src/com/Cayviel/RotatePiston/RotatePiston.java
+++ RotatePiston/src/com/Cayviel/RotatePiston/RotatePiston.java
@@ -1,9 +1,5 @@
 package com.Cayviel.RotatePiston;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.util.Properties;
 import java.util.logging.Logger;
 
 import org.bukkit.event.Event;
@@ -14,57 +10,57 @@ import org.bukkit.plugin.PluginManager;
 import com.nijiko.permissions.PermissionHandler;
 import com.nijikokun.bukkit.Permissions.Permissions;
 import org.bukkit.plugin.Plugin;
+import org.bukkit.util.config.Configuration;
 
-public class RotatePiston extends JavaPlugin{
+public class RotatePiston extends JavaPlugin {
 
 	private static InteractListener il = new InteractListener();
-    public static PermissionHandler permissionHandler;
-    public static boolean useop;
+	public static PermissionHandler permissionHandler;
 	public static Logger log = Logger.getLogger("Minecraft");
 
-	public static String directory = "plugins" + File.separator + "RotatePiston";	
-    public static File configFile = new File(directory + File.separator + "config.yml");
-    public static File serverproperties = new File("server.properties");
-	public static Properties properties = new Properties();
-	public static Rotateconfig rotateconfig = new Rotateconfig(configFile,directory);
+	private Configuration config;
+	static public Boolean opOnlyBoolean;
+	static public Boolean useWand;
+	static public Boolean usePerm = false;
+	static public String WandName;
 	
 	@Override
 	public void onDisable() {
 		getServer().getLogger().info("RotatePiston Disabled!");
-		
 	}
 
 	@Override
 	public void onEnable() {
+		// grab an instance of plugin manager
 		PluginManager pm = getServer().getPluginManager();
+		// register player interaction
 		pm.registerEvent(Event.Type.PLAYER_INTERACT, il, Priority.Normal, this);
-		getServer().getLogger().info("[RotatePiston]: Enabled!");
-		setupPermissions();
 
-		rotateconfig.configCheck();
-	    try {
-			FileInputStream inputfile = new FileInputStream(serverproperties);
-			properties.load(inputfile);
-			inputfile.close();
-		} catch (IOException eceptin) {
-			eceptin.printStackTrace();
-		}	
+		// grab our configuration file if there is none created it will create it for us with OP default true
+		config = getConfiguration();
+		useWand = config.getBoolean("Wand.Use a Wand", false);
+		WandName = config.getString("Wand.Wand", "AIR");
+		opOnlyBoolean = config.getBoolean("Op only (instead of Permissions)", true);
+		config.save();
+
+		// set up our permissions
+		if (!opOnlyBoolean && (getServer().getPluginManager().getPlugin("Permissions")!=null)){
+			setupPermissions();
+			usePerm = true;
+		}
+		// we have successfully enabled the plugin
+		log.info("[RotatePiston]: Enabled!");
 	}
-	
-	private void setupPermissions() { 
-		if (permissionHandler != null) { return; }
 
+	private void setupPermissions() {
+		// if our permissions handler isn't null we shall return
+		if (permissionHandler != null)
+			return;
+
+		// grab an instance of the permissions plugin
 		Plugin permissionsPlugin = getServer().getPluginManager().getPlugin("Permissions");
 
-		if (permissionsPlugin == null) {
-			if (rotateconfig.getOpOnly()){
-				log.info("Permission system not detected, defaulting to OP");
-				useop = true;
-				return;
-			}else{useop= false; return;}
-		}
 		permissionHandler = ((Permissions) permissionsPlugin).getHandler();
-		useop = false;
-		log.info("Found and will use plugin "+((Permissions)permissionsPlugin).getDescription().getFullName());
+		log.info("Found and will use plugin " + ((Permissions) permissionsPlugin).getDescription().getFullName());
 	}
 }
